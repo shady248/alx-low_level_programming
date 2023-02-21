76cd97bf5941ca8b41b7df27aa11344bebc14d42
shady248 <shadymamdouh482@gmail.com>
2023-02-21 06:32:28 -0800

    fixed the bug

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,17 @@
 /**
  * _isLower - checks for lowercase letters..
  *
- * Return: Always 0.
+ * @c: the character in ASCII mode..
+ *
+ * Return: 0 if false, 1 if true..
  */
+
 int _islower(int c)
 {
-	if ((97 <= c) | (c <= 122))
+	if ((97 <= c) && (c <= 122))
 		return (1);
 	else
 		return (0);
+	_putchar('\n');
 
 }
