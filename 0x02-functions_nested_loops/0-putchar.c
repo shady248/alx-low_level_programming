af6c3c090ad7aaf9fe3cecb8bf9d8ba87196a74a
shady248 <shadymamdouh482@gmail.com>
2023-02-21 05:40:44 -0800

    fixed it

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,13 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	int str[] = {95,112,117,116,99,104,97,114};
+	int size, count;
+	size = sizeof(str);
+	for (count = 0; count < size; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
 }
