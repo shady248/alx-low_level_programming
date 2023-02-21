026c1c408407cbc2d09f44ed51b124967f3cd8b3
shady248 <shadymamdouh482@gmail.com>
2023-02-21 06:14:14 -0800

    fixed the betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for( j = 97; j <= 122 ; j++)
+		for (j = 97; j <= 122 ; j++)
 		{
-			_putchar(j );
+			_putchar(j);
 		}
 	_putchar('\n');
 	}
