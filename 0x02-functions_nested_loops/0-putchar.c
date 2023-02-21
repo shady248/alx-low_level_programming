5c7fcc724ea219800aad99d1f2fb6b9bd483f91f
shady248 <shadymamdouh482@gmail.com>
2023-02-21 05:44:07 -0800

    testing again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	int str[] = {95,112,117,116,99,104,97,114};
 	int size, count;
-	size = sizeof(str);
+
+	size = sizeof(str) / sizeof(int);
 	for (count = 0; count < size; count++)
 	{
 		_putchar(str[count]);
