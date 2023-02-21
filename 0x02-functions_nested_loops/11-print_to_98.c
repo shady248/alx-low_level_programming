84c11a4246294630f1b0dd7ce401c2eaf7b8a0cd
shady248 <shadymamdouh482@gmail.com>
2023-02-21 07:49:46 -0800

    last task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	if (n = 0)
+		n = n+1;
+
+	for (n;n>=98;n++)
+	{
+		printf("%d\n",n);
+		return (0);
+	}
+}
