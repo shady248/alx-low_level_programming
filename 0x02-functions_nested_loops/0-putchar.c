8bf326cb3eccca3d859aace05eaae2380d8b02f4
shady248 <shadymamdouh482@gmail.com>
2023-02-21 05:04:15 -0800

    testing somethings

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the nested loops task in c 
