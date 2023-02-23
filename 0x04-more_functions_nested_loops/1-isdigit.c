18a248c609858f1b7cd9c9b3032f0409b8dea8d3
shady248 <shadymamdouh482@gmail.com>
2023-02-23 10:50:04 -0800

    more betty fixes

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
-/** _isdigit - checks for a didit
-  *
-  * @c: character in ASCII mode
-  *
-  * Return: 1 if true , 0 otherwise
-  */
+/**
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if (c >= 48 && c <= 57)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
