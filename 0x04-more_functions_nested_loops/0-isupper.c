c5243797e1693049680ad66d8304e2312268c173
shady248 <shadymamdouh482@gmail.com>
2023-02-23 10:36:16 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * -isupper - checks for uppercase character.
+ *
+ * @c: character in ASCII mode
+ *
+ * Return: 1 if uppercase , 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the folder for the more functions, more nested loops task
