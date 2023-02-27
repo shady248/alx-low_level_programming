67488857b5067c787b899ea5278b962e55c05589
shady248 <shadymamdouh482@gmail.com>
2023-02-27 09:31:26 -0800

    second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two intigers.
+ * @ temp : temporaroy variable.
+ * @a : pointer to the first argument.
+ * @b : pointer to the secod argument.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = *a;
+
+}
