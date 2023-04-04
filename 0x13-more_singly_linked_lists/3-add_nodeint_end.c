3c150f5fe60370d08b182a1a35ff4b50c0a6d56b
shady248 <shadymamdouh482@gmail.com>
2023-04-04 13:27:45 -0700

    Add node at the end task && README update

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+  * add_nodeint_end - adds a node at the end of the linked list
+  * @head: the list head
+  * @n: number of elements
+  *
+  * Return:  the address of the new element, or NULL if it failed
+  */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *tmp;
+
+	(void)temp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	tmp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+
+	return (*head);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -8,3 +8,5 @@ Files Table
 | lists.h       | The heaer file containing the prototypes of the project      |
 | 0-print\_listint.c |  prints all the elements of a listint\_t list      | 
 | 1-listint\_len.c | prints  the number of elements in a linked listint\_t list.|
+| 2-add\_nodeint.c |  adds a new node at the beginning of a listint\_t list. |
+| 3-add\_nodeint\_end.c | t adds a new node at the end of a listint\_t list. |
