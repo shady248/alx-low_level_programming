e0dc281e09aabc506842fc50096aa89ddccd4f8d
shady248 <shadymamdouh482@gmail.com>
2023-04-04 15:19:27 -0700

    [Fix] insert node int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,5 +36,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		*head = new;
 	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
 	return (new);
 }
