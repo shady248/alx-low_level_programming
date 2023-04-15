29d1c4f7a8716f07ea6051bdcfe5af92ef61898b
shady248 <shadymamdouh482@gmail.com>
2023-04-14 18:07:55 -0700

    first task & readme & headerfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads a text file and
+  * prints it to the POSIX standard output.
+  * @filename: the name of the file
+  * @letters: number of printed letters
+  *
+  * Return: numbers of letters printed, 0 if failed
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	size_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+	free(buf);
+	return (nwr);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int create_file(const char *filename, char *text_content);
+
+#endif
